52b3ef6d740df53b25e4bacab9dee880c9760906
Anna1112333 <anna_1112333@mail.ru>
2023-02-22 23:34:21 +0500

    Доработки

---
+++ task5001/task5001.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+
+class figure
+{
+private:
+    string name = "Фигура ";
+    int sides = 0;
+
+public:
+    void func1(int a, string b) {
+        this->sides = a;
+        this->name = b;
+    }
+    figure() { };
+    void print1()
+    {
+        cout << "Фигура: " << name << "сторон: " << sides << endl;
+    }
+};
+
+
+class triangle : public figure
+{
+public:
+    triangle() :figure() { func1(3, "Треугольник"); };
+};
+
+
+class quadrangle : public figure
+{
+public:
+    quadrangle() :figure() { func1(4, "Четырехугольник"); };
+};
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    cout << "Фигуры и колличество сторон: \n";
+    figure aa;
+    aa.print1();
+   
+    triangle vg;
+    vg.print1();
+
+    quadrangle fg;
+    fg.print1();
+
+    return 0;
+}
