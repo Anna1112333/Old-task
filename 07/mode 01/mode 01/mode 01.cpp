0d4c70956684ddbb2cfe3b102f3c995a433c9dac
Anna1112333 <anna_1112333@mail.ru>
2023-04-01 23:41:09 +0500

    2 задания07

---
+++ 07/mode 01/mode 01/mode 01.cpp
@@ -0,0 +1,34 @@
+﻿
+
+#include <iostream>
+#define MODE 0
+using namespace std;
+int summ(int x, int y) { return x + y; }
+
+int main()
+{
+	int x, y;
+	setlocale(LC_ALL, "RUS");
+#if defined MODE
+	{
+#if MODE == 0
+		cout << "Работаю в режиме треннировки.\n";
+#elif MODE==1 
+		{cout << "Введите первое число \n x=";
+		cin >> x;
+		cout << "Введите второе число \n y=" ;
+		cin >> y;
+		cout <<"x+y=" << summ(x, y);
+}
+
+#else 
+		cout << "Неизвестный режим. Завершение работы.";
+#endif 
+		 }
+#else 
+#error Mode not drfined.
+#endif
+}
+
+
+
---
+++ 07/sub02/sub02/sub02.cpp
@@ -0,0 +1,11 @@
+﻿#define sub(a,b) ((a)-(b))
+
+#include <iostream>
+
+int main()
+{
+    int a=5, b=6, c=11;
+    std::cout << sub(a,b)<<std::endl;
+    std::cout << sub(a, b) * c << std::endl;
+    std::cout << sub(a, b + c) * c << std::endl;
+}
